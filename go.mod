3d59059b5ea97e7fdd6215a1317b049d52813b9a
Roland Obasohan <67982367+rolandobas@users.noreply.github.com>
2020-07-16 20:48:30 +0200

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rolandobas/bastromweb
+
+go 1.14
+
+require github.com/elliotforbes/test-package v1.0.0 // indirect
---
+++ my-new-branch.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	sample "github.com/elliotforbes/test-package"
+)
+
+func main() {
+	fmt.Println("Hello world")
+
+	sample.MySampleFunction()
+}
